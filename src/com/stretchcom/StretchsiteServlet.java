e98d790093446b802e0d3decb2cc704b362efd01
Terry Roe <terryroe@gmail.com>
2011-11-09 15:27:41 -0500

    new gae application for stretchcom.com

---
+++ src/com/stretchcom/StretchsiteServlet.java
@@ -0,0 +1,13 @@
+package com.stretchcom;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class StretchsiteServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
